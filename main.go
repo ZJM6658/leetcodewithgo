f3acaf718a223a87ade3aa59a09516ab11e8d44f
guapi <guapi@yaomaitong.cn>
2022-02-03 23:46:28 +0800

    完成1-两数之和

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"leetcodewithgo/question"
 )
 
 func main() {
-	question.FindMinFibonacciNumbers(19)
+	//question.FindMinFibonacciNumbers(19)
+	fmt.Println(question.TwoSum([]int{2, 7, 8, 11, 15}, 26))
 }
