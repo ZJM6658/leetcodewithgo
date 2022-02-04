eb0289c6d824772cf3c419263de411509d983c4e
guapi <guapi@yaomaitong.cn>
2022-02-04 23:42:43 +0800

    完成1725-可以形成最大正方形的矩形数目

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	//question.FindMinFibonacciNumbers(19)
 	//a := question.TwoSum([]int{2, 7, 8, 11, 15}, 26))
-	a := question.LengthOfLongestSubstring("pwwkeabcpwwwwwwwww")
+	//a := question.LengthOfLongestSubstring("pwwkeabcpwwwwwwwww")
+	a := question.CountGoodRectangles([][]int{[]int{5, 8}, []int{3, 9}, []int{5, 12}, []int{16, 5}})
 	fmt.Println(a)
 }
