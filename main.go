c6af5f169037e9438edbdbc24a051799f20145b1
guapi <guapi@yaomaitong.cn>
2022-02-07 18:43:59 +0800

    完成5-最长回文子串

--- main.go
+++ main.go
@@ -11,8 +11,10 @@ func main() {
 	//a := question.LengthOfLongestSubstring("pwwkeabcpwwwwwwwww")
 	//a := question.CountGoodRectangles([][]int{[]int{5, 8}, []int{3, 9}, []int{5, 12}, []int{16, 5}})
 
-	a := question.FindMedianSortedArrays([]int{1, 2}, []int{3, 4})
+	//a := question.FindMedianSortedArrays([]int{1, 2}, []int{3, 4})
 	//a := question.FindMedianSortedArrays([]int{}, []int{2})
+
+	a := question.SumOfUnique([]int{1, 1, 2, 3, 2, 5, 7, 7, 7})
 	fmt.Println(a)
 
 }
