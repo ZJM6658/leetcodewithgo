67f40c4749eda460b38ba9e63cfe848b51905d82
guapi <guapi@yaomaitong.cn>
2022-02-11 10:55:19 +0800

    完成1984. 学生分数的最小差值

--- main.go
+++ main.go
@@ -17,7 +17,10 @@ func main() {
 	//a := question.SumOfUnique([]int{1, 1, 2, 3, 2, 5, 7, 7, 7})
 	//a := question.Convert("ZHUJIAMIN123", 3)
 
-	a := question.SimplifiedFractions(4)
+	//a := question.SimplifiedFractions(4)
+
+	//a := question.MinimumDifference([]int{9, 4, 1, 7}, 2)
+	a := question.MinimumDifference([]int{87063, 61094, 44530, 21297, 95857, 93551, 9918}, 6)
 
 	fmt.Println(a)
 
