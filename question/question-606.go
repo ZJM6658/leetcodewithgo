e4497de4611673a04c0ba16c3768c5a951dd0b82
guapi <guapi@yaomaitong.cn>
2022-03-20 11:08:28 +0800

    606. 根据二叉树创建字符串

---
+++ question/question-606.go
@@ -0,0 +1,64 @@
+package question
+
+import (
+	"fmt"
+	"strconv"
+)
+
+/**
+ * Definition for a binary tree node.
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func tree2str(root *TreeNode) string {
+	switch {
+	case root == nil:
+		return ""
+	case root.Left == nil && root.Right == nil:
+		return strconv.Itoa(root.Val)
+	case root.Right == nil:
+		return fmt.Sprintf("%d(%s)", root.Val, tree2str(root.Left))
+	default:
+		return fmt.Sprintf("%d(%s)(%s)", root.Val, tree2str(root.Left), tree2str(root.Right))
+	}
+}
+
+/*
+606. 根据二叉树创建字符串
+你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+
+空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+
+示例 1:
+
+输入: 二叉树: [1,2,3,4]
+       1
+     /   \
+    2     3
+   /
+  4
+
+输出: "1(2(4))(3)"
+
+解释: 原本将是“1(2(4)())(3())”，
+在你省略所有不必要的空括号对之后，
+它将是“1(2(4))(3)”。
+示例 2:
+
+输入: 二叉树: [1,2,3,null,4]
+       1
+     /   \
+    2     3
+     \
+      4
+
+输出: "1(2()(4))(3)"
+
+解释: 和第一个示例相似，
+除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
+*/
